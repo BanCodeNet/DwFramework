64ca0517d071a31baf8be2bd7184cb3496c7c38d
DwGoing <jianghy1209@163.com>
2021-11-17 16:43:09 +0800

    🦄 refactor: 调整基本异常结构

---
+++ src/DwFramework.Core/Models/Exception/BaseException.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace DwFramework.Core
+{
+    public abstract class BaseException : Exception
+    {
+        public int Code { get; init; }
+
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="code"></param>
+        /// <param name="message"></param>
+        /// <param name="innerException"></param>
+        public BaseException(int code, string message = null, Exception innerException = null) : base(message, innerException)
+        {
+            Code = code;
+        }
+    }
+}
--- src/DwFramework.Core/Models/Exception/NotFoundException.cs
+++ src/DwFramework.Core/Models/Exception/NotFoundException.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace DwFramework.Core
 {
-    public sealed class NotFoundException : ExceptionBase
+    public sealed class NotFoundException : BaseException
     {
         public string FoundThings { get; init; }
 
