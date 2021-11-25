8d6d33278ec2abb03c1d63c619589b3b15f88c01
DwGoing <jianghy1209@163.com>
2021-11-25 14:56:40 +0800

    🦄 refactor: 重构SHA内部实现

---
+++ src/DwFramework.Core/Plugins/Encrypt/SHA/SHA256.cs
@@ -0,0 +1,45 @@
+using System.Text;
+using System.Security.Cryptography;
+
+namespace DwFramework.Core.Encrypt;
+
+public static class SHA
+{
+    /// <summary>
+    /// 编码
+    /// </summary>
+    /// <param name="type"></param>
+    /// <param name="data"></param>
+    /// <returns></returns>
+    public static string Encode(SHAType type, byte[] data)
+    {
+        byte[] bytes = null;
+        switch (type)
+        {
+            case SHAType.SHA1:
+                var sha1 = SHA1.Create();
+                bytes = sha1.ComputeHash(data, 0, data.Length);
+                sha1.Dispose();
+                break;
+            case SHAType.SHA256:
+                var sha256 = SHA256.Create();
+                bytes = sha256.ComputeHash(data, 0, data.Length);
+                sha256.Dispose();
+                break;
+            case SHAType.SHA384:
+                var sha384 = SHA384.Create();
+                bytes = sha384.ComputeHash(data, 0, data.Length);
+                sha384.Dispose();
+                break;
+            case SHAType.SHA512:
+                var sha512 = SHA512.Create();
+                bytes = sha512.ComputeHash(data, 0, data.Length);
+                sha512.Dispose();
+                break;
+        }
+        if (bytes == null || bytes.Length <= 0) return null;
+        var builder = new StringBuilder();
+        for (int i = 0; i < bytes.Length; i++) builder.Append(bytes[i].ToString("x2"));
+        return builder.ToString();
+    }
+}
---
+++ src/DwFramework.Core/Plugins/Encrypt/SHA/SHAType.cs
@@ -0,0 +1,9 @@
+namespace DwFramework.Core.Encrypt;
+
+public enum SHAType
+{
+    SHA1,
+    SHA256,
+    SHA384,
+    SHA512
+}
