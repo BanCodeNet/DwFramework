c91feed8946b4bf9d5cb587fb12af2864cb3d424
DwGoing <jianghy1209@163.com>
2021-11-25 16:45:21 +0800

    📃 docs: 迁移文档到wiki

--- example/RabbitMQExample/Program.cs
+++ example/RabbitMQExample/Program.cs
@@ -16,16 +16,16 @@ class Program
         host.OnHostStarted += p =>
         {
             var s = p.GetRabbitMQ();
-                // 创建交换机
-                s.ExchangeDeclare("exchange.direct", ExchangeType.Direct);
-                // 创建队列
-                s.QueueDeclare("queueA", true);
-                // 绑定
-                s.QueueBind("queueA", "exchange.direct", "A");
-                // 订阅
-                s.Subscribe("queueA", true, (m, a) => Console.WriteLine(Encoding.UTF8.GetString(a.Body.ToArray())));
-                // 发布消息
-                s.Publish(new
+            // 创建交换机
+            s.ExchangeDeclare("exchange.direct", ExchangeType.Direct);
+            // 创建队列
+            s.QueueDeclare("queueA", true);
+            // 绑定
+            s.QueueBind("queueA", "exchange.direct", "A");
+            // 订阅
+            s.Subscribe("queueA", true, (m, a) => Console.WriteLine(Encoding.UTF8.GetString(a.Body.ToArray())));
+            // 发布消息
+            s.Publish(new
             {
                 Id = 1,
                 Name = "Joy"
--- example/WebExample/Program.cs
+++ example/WebExample/Program.cs
@@ -24,8 +24,6 @@ class Program
         host.ConfigureWeb(configuration, builder => builder.UseStartup<Startup>(), "web");
         // host.ConfigureSocket(configuration, "tcp");
         // host.ConfigureSocket(configuration, "udp");
-        host.ConfigureLogging(builder => builder.UserNLog());
-
         host.OnHostStarted += p =>
         {
             var web = p.GetWeb();
@@ -33,32 +31,32 @@ class Program
             web.OnWebSocketReceive += (c, a) => Console.WriteLine($"{c.ID} {Encoding.UTF8.GetString(a.Data)}");
             web.OnWebSocketClose += (c, a) => Console.WriteLine($"{c.ID} 断开连接");
 
-                // var tcp = p.GetTcp();
-                // tcp.OnConnect += (c, a) => Console.WriteLine($"{c.ID} connected");
-                // tcp.OnReceive += (c, a) =>
-                // {
-                //     Console.WriteLine($"{c.ID} received {Encoding.UTF8.GetString(a.Data)}");
-                //     var body = @"<h1>Hello World</h1><span>XXXXXXX</span>";
-                //     _ = c.SendAsync(Encoding.UTF8.GetBytes(
-                //         "HTTP/1.1 200 OK\r\n"
-                //         + "Date: Sat, 31 Dec 2005 23:59:59 GMT\r\n"
-                //         + "Content-Type: text/html;charset=UTF8\r\n"
-                //         + $"Content-Length: {Encoding.UTF8.GetByteCount(body)}\r\n\n"
-                //         + $"{body}"
-                //     ));
-                // };
-                // tcp.OnSend += (c, a) => Console.WriteLine($"{c.ID} sent {Encoding.UTF8.GetString(a.Data)}");
-                // tcp.OnClose += (c, a) => Console.WriteLine($"{c.ID} closed");
+            // var tcp = p.GetTcp();
+            // tcp.OnConnect += (c, a) => Console.WriteLine($"{c.ID} connected");
+            // tcp.OnReceive += (c, a) =>
+            // {
+            //     Console.WriteLine($"{c.ID} received {Encoding.UTF8.GetString(a.Data)}");
+            //     var body = @"<h1>Hello World</h1><span>XXXXXXX</span>";
+            //     _ = c.SendAsync(Encoding.UTF8.GetBytes(
+            //         "HTTP/1.1 200 OK\r\n"
+            //         + "Date: Sat, 31 Dec 2005 23:59:59 GMT\r\n"
+            //         + "Content-Type: text/html;charset=UTF8\r\n"
+            //         + $"Content-Length: {Encoding.UTF8.GetByteCount(body)}\r\n\n"
+            //         + $"{body}"
+            //     ));
+            // };
+            // tcp.OnSend += (c, a) => Console.WriteLine($"{c.ID} sent {Encoding.UTF8.GetString(a.Data)}");
+            // tcp.OnClose += (c, a) => Console.WriteLine($"{c.ID} closed");
 
 
-                // var udp = p.GetUdp();
-                // udp.OnReceive += (c, a) =>
-                // {
-                //     Console.WriteLine($"{c} received {Encoding.UTF8.GetString(a.Data)}");
-                //     udp.SendTo(Encoding.UTF8.GetBytes("World"), c);
-                // };
-                // udp.OnSend += (c, a) => Console.WriteLine($"{c} sent {Encoding.UTF8.GetString(a.Data)}");
-            };
+            // var udp = p.GetUdp();
+            // udp.OnReceive += (c, a) =>
+            // {
+            //     Console.WriteLine($"{c} received {Encoding.UTF8.GetString(a.Data)}");
+            //     udp.SendTo(Encoding.UTF8.GetBytes("World"), c);
+            // };
+            // udp.OnSend += (c, a) => Console.WriteLine($"{c} sent {Encoding.UTF8.GetString(a.Data)}");
+        };
         await host.RunAsync();
     }
 }
@@ -118,10 +116,10 @@ public sealed class Startup
         });
         services.AddControllers().AddJsonOptions(options =>
         {
-                //不使用驼峰样式的key
-                options.JsonSerializerOptions.PropertyNamingPolicy = null;
-                //不使用驼峰样式的key
-                options.JsonSerializerOptions.DictionaryKeyPolicy = null;
+            //不使用驼峰样式的key
+            options.JsonSerializerOptions.PropertyNamingPolicy = null;
+            //不使用驼峰样式的key
+            options.JsonSerializerOptions.DictionaryKeyPolicy = null;
         });
         services.AddMvc(options => options.UseRoutePrefix("api"));
         services.AddRpcImplements();
