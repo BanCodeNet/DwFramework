a3e649dbef653dbea9fc0730a0793c967a31e033
DwGoing <jianghy1209@163.com>
2021-11-25 14:19:48 +0800

    🦄 refactor: 重构AES内部实现

---
+++ src/DwFramework.Core/Plugins/Encrypt/AES/AES.cs
@@ -0,0 +1,58 @@
+using System.Security.Cryptography;
+
+namespace DwFramework.Core.Encrypt;
+
+public static class AES
+{
+    /// <summary>
+    /// 加密
+    /// </summary>
+    /// <param name="type"></param>
+    /// <param name="data"></param>
+    /// <param name="key"></param>
+    /// <param name="iv"></param>
+    /// <param name="mode"></param>
+    /// <param name="padding"></param>
+    /// <returns></returns>
+    public static byte[] Encrypt(AESType type, byte[] data, byte[] key, byte[] iv = null, CipherMode mode = CipherMode.CBC, PaddingMode padding = PaddingMode.None)
+    {
+        if (data == null || data.Length <= 0) throw new Exception("data为空");
+        if (key == null) throw new Exception("key为空");
+        var keySize = (int)type;
+        if (key.Length != keySize / 8) Array.Resize(ref key, keySize / 8);
+        using var aes = Aes.Create();
+        aes.KeySize = keySize;
+        aes.Mode = mode;
+        aes.Padding = padding;
+        if (iv == null) iv = new byte[aes.BlockSize / 8];
+        else Array.Resize(ref iv, aes.BlockSize / 8);
+        using var encryptor = aes.CreateEncryptor(key, iv);
+        return encryptor.TransformFinalBlock(data, 0, data.Length);
+    }
+
+    /// <summary>
+    /// 解密
+    /// </summary>
+    /// <param name="type"></param>
+    /// <param name="data"></param>
+    /// <param name="key"></param>
+    /// <param name="iv"></param>
+    /// <param name="mode"></param>
+    /// <param name="padding"></param>
+    /// <returns></returns>
+    public static byte[] Decrypt(AESType type, byte[] data, byte[] key, byte[] iv = null, CipherMode mode = CipherMode.CBC, PaddingMode padding = PaddingMode.None)
+    {
+        if (data == null || data.Length <= 0) throw new Exception("data为空");
+        if (key == null) throw new Exception("key为空");
+        var keySize = (int)type;
+        if (key.Length != keySize / 8) Array.Resize(ref key, keySize / 8);
+        using var aes = Aes.Create();
+        aes.KeySize = keySize;
+        aes.Mode = mode;
+        aes.Padding = padding;
+        if (iv == null) iv = new byte[aes.BlockSize / 8];
+        else Array.Resize(ref iv, aes.BlockSize / 8);
+        using var decryptor = aes.CreateDecryptor(key, iv);
+        return decryptor.TransformFinalBlock(data, 0, data.Length);
+    }
+}
---
+++ src/DwFramework.Core/Plugins/Encrypt/AES/AESType.cs
@@ -0,0 +1,8 @@
+namespace DwFramework.Core.Encrypt;
+
+public enum AESType
+{
+    AES_128 = 128,
+    AES_192 = 192,
+    AES_256 = 256,
+}
