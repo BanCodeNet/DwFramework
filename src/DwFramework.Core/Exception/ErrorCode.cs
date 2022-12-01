3e0bd458cd55549c8bb16f5a2e27bbadd6d771a5
DwGoing <jianghy1209@163.com>
2022-12-01 18:28:24 +0800

    🦄 refactor: 重构随机生成器

--- src/DwFramework.Core/Exception/ErrorCode.cs
+++ src/DwFramework.Core/Exception/ErrorCode.cs
@@ -3,4 +3,5 @@ namespace DwFramework;
 public static class ErrorCode
 {
     public static int InternalError { get; } = 500;
+    public static int ParameterError { get; } = 501;
 }
---
+++ src/DwFramework.Extensions/Common/RandomGenerator.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+
+namespace DwFramework.Extensions;
+
+public static class RandomGenerator
+{
+    /// <summary>
+    /// 随机数生成器
+    /// </summary>
+    /// <returns></returns>
+    public static Random GetRandom()
+    {
+        var seed = BitConverter.ToInt64(Guid.NewGuid().ToByteArray());
+        return new Random((int)seed);
+    }
+
+    /// <summary>
+    /// 生成随机数
+    /// </summary>
+    /// <param name="start"></param>
+    /// <param name="end"></param>
+    /// <returns></returns>
+    public static int RandomNumber(int start, int end)
+    {
+        if (start <= end) throw new ExceptionWithCode(ErrorCode.ParameterError, "end must great then start");
+        var random = GetRandom();
+        return random.Next(start, end);
+    }
+
+    /// <summary>
+    /// 生成随机数
+    /// </summary>
+    /// <param name="start"></param>
+    /// <param name="end"></param>
+    /// <returns></returns>
+    public static double RandomNumber(double start, double end)
+    {
+        if (start <= end) throw new ExceptionWithCode(ErrorCode.ParameterError, "end must great then start");
+        var random = GetRandom();
+        return (end - start) * random.NextDouble() + start;
+    }
+
+    /// <summary>
+    /// 生成随机字符串
+    /// </summary>
+    /// <param name="length"></param>
+    /// <param name="chars"></param>
+    /// <returns></returns>
+    public static string RandomString(int length, string chars = null)
+    {
+        chars ??= @"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        var builder = new StringBuilder();
+        for (int i = 0; i < length; i++) builder.Append(chars[RandomNumber(0, chars.Length)]);
+        return builder.ToString();
+    }
+}
