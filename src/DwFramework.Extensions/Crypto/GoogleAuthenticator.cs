2003030ff5bce7b147809ffebfef747b46f4bc9d
DwGoing <jianghy1209@163.com>
2022-12-01 16:37:53 +0800

    🦄 refactor: 移除冗余Crypto扩展

---
+++ src/DwFramework.Extensions/Crypto/GoogleAuthenticator.cs
@@ -0,0 +1,61 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace DwFramework.Extensions.Crypto;
+
+public static class GoogleAuthenticator
+{
+    /// <summary>
+    /// 生成秘钥
+    /// </summary>
+    /// <returns></returns>
+    public static string GenerateKey(string key)
+    {
+        return Encoding.UTF8.GetBytes(key).ToBase32().TrimEnd('=');
+    }
+
+    /// <summary>
+    /// 按照次数生成哈希编码
+    /// </summary>
+    /// <param name="secret">秘钥</param>
+    /// <param name="iterationNumber">迭代次数</param>
+    /// <param name="digits">生成位数</param>
+    /// <returns>返回验证码</returns>
+    private static string GenerateHashedCode(string secret, long iterationNumber, int digits = 6)
+    {
+        var counter = BitConverter.GetBytes(iterationNumber);
+        if (BitConverter.IsLittleEndian)
+            Array.Reverse(counter);
+        var key = Encoding.ASCII.GetBytes(secret);
+        var hmac = new HMACSHA1(key);
+        var hash = hmac.ComputeHash(counter);
+        var offset = hash[hash.Length - 1] & 0xf;
+        var binary = ((hash[offset] & 0x7f) << 24)
+            | ((hash[offset + 1] & 0xff) << 16)
+            | ((hash[offset + 2] & 0xff) << 8)
+            | (hash[offset + 3] & 0xff);
+        var password = binary % (int)Math.Pow(10, digits);
+        return password.ToString(new string('0', digits));
+    }
+
+    /// <summary>
+    /// 生成认证码
+    /// </summary>
+    /// <returns>返回验证码</returns>
+    public static string GenerateCode(string key, int duration = 30)
+    {
+        var counter = (long)(DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalSeconds / duration;
+        return GenerateHashedCode(key, counter);
+    }
+
+    /// <summary>
+    /// 验证
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="code"></param>
+    /// <returns></returns>
+    public static bool VerifyCode(string key, string code)
+    {
+        return code == GenerateCode(key);
+    }
+}
