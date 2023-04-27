a1a01d399c9f4acca099fcc9407eb71848d8e23c
DwGoing <jianghy1209@163.com>
2023-04-27 14:50:20 +0800

    🛠 miscellaneous: 消除警告

--- src/DwFramework.Extensions/Web/EventStreamEvent.cs
+++ src/DwFramework.Extensions/Web/EventStreamEvent.cs
@@ -2,8 +2,8 @@ namespace DwFramework.Extensions;
 
 public record class EventStreamEvent
 {
-    public string Id { get; init; }
-    public string Event { get; init; }
+    public string? Id { get; init; }
+    public string? Event { get; init; }
     public int? Retry { get; init; }
-    public IAsyncEnumerable<object> Data { get; init; }
+    public IAsyncEnumerable<object>? Data { get; init; }
 }
